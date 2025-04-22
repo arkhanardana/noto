a638a30317ae98559535b0cdc6e52f2471c5f1dd
ArifRTzy <arifr0751@gmail.com>
2025-04-22 12:08:30 +0700

    feat: add format date

---
+++ components/ui/format-date.tsx
@@ -0,0 +1,29 @@
+export function getDaySuffix(day) {
+    if (day >= 11 && day <= 13) {
+      return "th";
+    }
+    switch (day % 10) {
+      case 1:
+        return "st";
+      case 2:
+        return "nd";
+      case 3:
+        return "rd";
+      default:
+        return "th";
+    }
+}
+
+const FormatDate = ({ dateString }) => {
+  const date = new Date(dateString);
+
+  const day = date.getDate();
+  const month = date.toLocaleString("default", { month: "long" });
+  const year = date.getFullYear();
+
+  const suffix = getDaySuffix(day);
+
+  return `${day}${suffix} ${month} ${year}`;
+}
+
+export default FormatDate;

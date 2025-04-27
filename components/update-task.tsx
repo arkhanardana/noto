df82ecb0ebf5fb34bfac10a38e5ee89f5d33e6e7
ArifRTzy <arifr0751@gmail.com>
2025-04-27 23:41:29 +0700

    feat: add update task

--- components/update-task.tsx
+++ components/update-task.tsx
@@ -1,29 +1,67 @@
-type UserProps = {
-    titles: string,
-    descs: string,
-    priorities: string,
-    dates: string,
+import { toast } from "sonner";
+
+type Status = "PROGRESS" | "COMPLETED";
+type Priority = "LOW" | "MEDIUM" | "HIGH";
+
+const UpdateTask = async (
+  titles: string,
+  descriptions: string,
+  statuses: Status,
+  priorities: Priority,
+  deadlines: string,
+  onClose: () => void,
+  id: number
+) => {
+  try {
+    const res = await fetch(`http://localhost:3000/api/todos/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: titles,
+        description: descriptions,
+        status: statuses,
+        priority: priorities,
+        deadline: deadlines,
+      }),
+    });
+    const data = await res.json();
+
+    console.log(data);
+    toast.success("Successfully update task")
+    return data;
+  } catch (error) {
+    console.log(error);
+    toast.error("Failed to update task")
+  }finally{
+    onClose()
+  }
+};
+
+export const UpdateStatus = async (
+    statuses: Status,
     id: number
+  ) => {
+    try {
+      const res = await fetch(`http://localhost:3000/api/todos/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          status: statuses,
+        }),
+      });
+      const data = await res.json();
+  
+      console.log(data);
+      toast.success("Successfully update task's staus")
+      return data;
+    } catch (error) {
+      console.log(error);
+      toast.error("Failed to update task's status")
+    }
   };
 
-const UpdateTask = async ({titles, descs, priorities, dates, id}: UserProps) => {
-    try {
-        const res = await fetch(`http://localhost:3000/api/todos/${id}`, {
-            method: "PUT",
-            body: JSON.stringify({
-                "title": titles,
-                "desc" : descs,
-                "priority": priorities,
-                "deadline": dates
-            })
-        })
-        const data = await  res.json()
-
-        console.log(data)
-        return data
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-export default UpdateTask
+export default UpdateTask;
